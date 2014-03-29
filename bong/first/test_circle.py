174f8e5d92bb3790b7c81a884ed16b3f7d6ab445
bongyong <fenixyong@gmail.com>
2014-03-29 17:15:30 +0900

    영화관 수입 계산

--- bong/first/test_circle.py
+++ bong/first/test_circle.py
@@ -1,7 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
 def circle(radius):
+    """원의 넓이를 구하는 함수"""
     return pow(radius, 2) * 3.14
+
+
+def donut(in_circle, out_circle):
+    return circle(out_circle) - circle(in_circle)
 
 
 def test_circle2():
     assert 12.56 == circle(2)
 
+
+def test_donut():
+    assert 37.68  == donut(2, 4)

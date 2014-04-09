971bfc62ec108b74f39360c666f7e4875b1b398b
kyungsun <mysinta@naver.com>
2014-04-09 04:59:11 +0000

    homwork01 -1

--- kyungsun/homework/homework01.py
+++ kyungsun/homework/homework01.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 def test_interest():
-    assert interest(500)==20
-    assert interest(1000)==40
-    assert interest(1500)==67.5
-    assert interest(5000)==250
-    assert interest(6000)==300
+    assert interest(500 )== 20
+    assert interest(1000) == 40
+    assert interest(1500) == 67.5
+    assert interest(5000) == 250
+    assert interest(6000) == 300
 
 def interest(deposit):
-    if(deposit<=1000):
+    if deposit<=1000:
         return deposit*0.04
-    elif(1000<deposit<5000):
+    elif 1000<deposit<5000:
         return deposit*0.045
-    elif(5000<=deposit):
+    elif 5000<=deposit:
         return deposit*0.05
 
 

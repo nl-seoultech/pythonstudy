51ed777d3c9998272f2873fd8e43992d69baf698
noahcles <tanesia_cou@naver.com>
2014-04-16 20:18:24 +0900

    #13 Function : data type discriminator

---
+++ gong/cherry_home_work/test_type_discriminator.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+def type_discriminate(data):
+    """ method to discriminate the input data type """
+
+    if isinstance(data, bool):
+        return "bool"
+    
+    elif isinstance(data, int):
+        return "int"
+
+    elif isinstance(data, float):
+        return "float"
+
+    elif isinstance(data, str):
+        return "str"
+
+    elif isinstance(data, unicode):
+        return "unicode"
+
+    elif isinstance(data, type):
+        return "type"
+
+    elif isinstance(data, NoneType):
+        return "NoneType"
+
+    else:
+        return "none of data"
+
+
+
+def test_type_discriminate():
+    assert "bool"	== type_discriminate(False)
+    assert "int" 	== type_discriminate(1)
+    assert "float"	== type_discriminate(1.1)
+    assert "str" 	== type_discriminate("abc")
+    assert "unicode"	== type_discriminate(u"tiffany")
+    assert "type"	== type_discriminate(str)
+
+    """
+    assert "NoneType"	== type_discriminate(None)
+
+    ##in repple, "type(None)" print "NoneType", but this type is undefined.
+    """

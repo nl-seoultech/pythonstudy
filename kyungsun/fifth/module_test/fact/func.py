5dc6a13d6f0c923cd2bd902f7ba44abd12e1d394
kyungsun <mysinta@naver.com>
2014-05-09 08:45:52 +0000

    'gitgit'

---
+++ kyungsun/fifth/module_test/fact/func.py
@@ -0,0 +1,5 @@
+def factorial(n):
+    r = 1
+    for x in range(1, n + 1):
+        r = r * x
+    return r
---
+++ kyungsun/fifth/module_test/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='module_test',
+      version='0.0.1',
+      author="Kyungsun Park",
+      author_email="asinta@navr.com",
+      install_requires=['pytest==2.3.5'],
+      packages=find_packages())
